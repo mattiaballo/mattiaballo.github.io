ef890300960cffd4ffb030dabd3edbd326f1a3ae
mballo <mattia.ballo@outlook.com>
2018-03-03 13:19:28 +0100

    add all dependencies and update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,120 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
+// PROJECT SPECIFIC SETTINGS
+// #FONTS
+var fonts_src = [
+  'node_modules/font-awesome/fonts/*.*'
+];
+
+var fonts_dest = 'dist/fonts';
+
+// #CSS
+var css_src = [
+  'node_modules/bootstrap/dist/css/bootstrap.min.css',
+  'node_modules/animate.css/animate.min.css',
+  'node_modules/font-awesome/css/font-awesome.min.css',
+  'node_modules/magnific-popup/dist/magnific-popup.css'
+];
+
+var css_dest = 'dist/css';
+
+// #Javascript
+
+var js_src = [
+  'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+  'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
+  'node_modules/jquery-parallax.js/parallax.min.js',
+  'node_modules/smooth-scroll/dist/js/smooth-scroll.min.js',
+  'node_modules/wowjs/dist/wow.min.js',
+  'src/js/script.js'
+];
+
+var js_dest = 'dist/js';
+
+// #SASS files
+
+var scss_src = [
+  'src/scss/theme.scss'
+];
+
+// #IMAGES files
+
+var img_src = [
+  'src/images/*.jpg',
+  'src/images/*.png'
+];
+
+var img_dest = 'dist/images';
+
+// #HTML pages
+
+var html_src = [
+  'src/index.html'
+];
+
+var html_dest = 'dist';
+
+
+// SCRIPTS GULP
+
 // Compile Sass into CSS & auto-inject into browser
 gulp.task('sass', function () {
-  return gulp.src(['src/scss/deck.scss', 'src/scss/spectre-theme.scss', 'src/scss/style.scss'])
+  return gulp.src(scss_src)
     .pipe(sass())
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest(css_dest))
     .pipe(browserSync.stream());
 });
 
 // Move Js files to /src/js folder
 gulp.task('js', function () {
-  return gulp.src([
-      'node_modules/deck-of-cards/dist/deck.min.js'
-    ])
-    .pipe(gulp.dest('src/js'))
+  return gulp.src(js_src)
+    .pipe(gulp.dest(js_dest))
     .pipe(browserSync.stream());
 });
 
-// Move CSS files to /src/css folder
+// Move CSS files to dist folder
 gulp.task('CSS', function () {
-  return gulp.src(['node_modules/material-colors/dist/colors.css'])
-    .pipe(gulp.dest('src/css'))
+  return gulp.src(css_src)
+    .pipe(gulp.dest(css_dest))
+    .pipe(browserSync.stream());
+});
+
+// Move Fonts files to dist folder
+gulp.task('fonts', function () {
+  return gulp.src(fonts_src)
+    .pipe(gulp.dest(fonts_dest))
+    .pipe(browserSync.stream());
+});
+
+// Move images files to dist folder
+gulp.task('images', function () {
+  return gulp.src(img_src)
+    .pipe(gulp.dest(img_dest))
+    .pipe(browserSync.stream());
+});
+
+// Move HTML files to dist folder
+gulp.task('html', function () {
+  return gulp.src(html_src)
+    .pipe(gulp.dest(html_dest))
     .pipe(browserSync.stream());
 });
 
 // Static server + watching scss/html/js files
-gulp.task('serve', ['sass'], function () {
+gulp.task('serve', function () {
 
   browserSync.init({
-    server: './src'
+    server: './dist'
   });
 
   gulp.watch(['src/scss/*.scss'], ['sass']);
-  gulp.watch(['src/*.html', 'src/js/*.js', 'src/css/*.css']).on('change', browserSync.reload);
+  gulp.watch(['dist/*.html', 'dist/js/*.js', 'dist/css/*.css']).on('change', browserSync.reload);
 })
 
-gulp.task('default', ['js', 'CSS', 'serve']);
+// build task
+gulp.task('build', ['js', 'CSS', 'fonts', 'images', 'sass', 'html']);
+
+//default task
+gulp.task('default', ['build', 'serve']);
